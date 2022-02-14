58a1851791a161c28ff7b8699bff372ce41a6e00
tavisca-Vvijayakumar <89015369+tavisca-Vvijayakumar@users.noreply.github.com>
2022-02-14 11:45:55 +0530

    Update usercontext.js

--- js/usercontext.js
+++ js/usercontext.js
@@ -9,7 +9,6 @@ var userContextConfiguration = {
         var bytes = CryptoJS.AES.decrypt(userContextObject, 'secret key 123');
         var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8)); 
         usercontext = decryptedData;
-        document.getElementById("teamplatename").value = usercontext.TemplateName;
         return "Ok";
     }
 }
